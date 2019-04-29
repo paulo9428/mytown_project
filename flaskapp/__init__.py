17e6fc80cb1e88dcd543a813d5bbd920a4c72101
paulo9428 <paulo9428@naver.com>
2019-04-29 16:44:52 +0900

    2019-04-29

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,6 +3,7 @@ from flask import session, render_template, request
 from datetime import datetime, date
 from flask import g , Response, make_response, url_for
 from pymongo import MongoClient
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 app.debug = True
@@ -39,6 +40,26 @@ def mongoTest():
     results = collection.find()
     client.close()
     return render_template('mongo.html', data=results)
+
+
+#업로드 HTML 렌더링
+@app.route('/upload')
+def render_file():
+   return render_template('upload.html')
+
+#파일 업로드 처리
+@app.route('/fileUpload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      
+      f = request.files['file']
+      #저장할 경로 + 파일명
+      f.save('flaskapp/uploads/' + secure_filename(f.filename))
+      return 'uploads 디렉토리 -> 파일 업로드 성공!'
+
+if __name__ == '__main__':
+    #서버 실행
+   app.run(debug = True)
 
  
 
